5c053a3dff0fab2fe6234e5110c418cf594a2af4
Ankur Bhat <ankurbt@hangon.ccs.neu.edu>
2014-09-13 15:41:43 -0400

    initial sorce code

---
+++ Debug/sources.mk
@@ -0,0 +1,31 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+O_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+OBJ_SRCS := 
+ASM_SRCS := 
+OBJS := 
+C_DEPS := 
+EXECUTABLES := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+threads \
+tests/vm \
+tests/userprog \
+tests/userprog/no-vm \
+tests/threads \
+tests \
+tests/internal \
+tests/filesys \
+tests/filesys/extended \
+tests/filesys/base \
+lib \
+lib/user \
+lib/kernel \
+examples \
+devices \
+
